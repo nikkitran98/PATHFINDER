af8191a19b8de0329466152d847de857502f9e3d
Nikki <theninjanikki@nikkis-mbp.3500.dhcp.asu.edu>
2018-09-30 14:53:00 -0700

    print menu and README overview

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 PATHFINDER
+An app that improves project management planning and increase efficiency and accuracy in project planning delivery times. Users are able to log activities and expected completion times to find the most efficient path to project completion.
 
 Creators:
 Nikki Tran
--- src/PATHFINDER.java
+++ src/PATHFINDER.java
@@ -1,6 +1,20 @@
 import java.util.*;
+
 public class PATHFINDER {
 	public static void main (String []args) {
-		
+		printMenu();
+	
+	}
+	
+	public static void printMenu () {
+		System.out.print("Choice\t\tAction\n" +
+                "------\t\t------\n" +
+                "A\t\tAdd\n" +
+                "C\t\tCalculate\n" +
+                "D\t\tDelete\n" +
+                "R\t\tRestart\n" +
+                "O\t\tAbout\n" +
+                "Q\t\tQuit\n" +
+                "?\t\tDisplay Help\n\n");
 	}
 }
