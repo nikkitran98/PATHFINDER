96fb24cf89eff7477155bb20592529ca4f131ad2
Nikki <theninjanikki@nikkis-mbp.3500.dhcp.asu.edu>
2018-10-04 00:26:34 -0700

    added GUI frame and components

--- src/PATHFINDER.java
+++ src/PATHFINDER.java
@@ -1,9 +1,26 @@
 import java.util.*;
+import javax.swing.*;
+import java.awt.*; 
 
-public class PATHFINDER {
+public class PATHFINDER extends JApplet {
+	public void init () {
+		Container content = getContentPane();
+		
+		Button addButton = new Button("Add");
+		Button restartButton = new Button("Restart");
+		Button calcButton = new Button("Calculate");
+		Button helpButton = new Button("?");
+		Button aboutButton = new Button("A");
+		
+		content.add(addButton);
+		content.add(restartButton);
+		content.add(calcButton);
+		content.add(helpButton);
+		content.add(aboutButton);
+	}
+	
 	public static void main (String []args) {
 		printMenu();
-	
 	}
 	
 	public static void printMenu () {
